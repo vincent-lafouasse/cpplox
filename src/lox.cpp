aae104814e7ed557b91a6265f91e23905ea98e39
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-05-02 20:04:08 +0200

    parse sysargs

--- src/lox.cpp
+++ src/lox.cpp
@@ -1,7 +1,45 @@
 #include "lox.h"
 
+#include <fstream>
 #include <iostream>
+#include <sstream>
+
+static std::string load_file(std::string path);
 
 void Lox::main(int argc, char* argv[]) {
-  std::cout << "running cpplox" << std::endl;
+  if (argc > 2) {
+    std::cout << "Usage: cpplox [script]" << std::endl;
+    exit(64);
+  } else if (argc == 2) {
+    run_file(argv[1]);
+  } else {
+    run_prompt();
+  }
+}
+
+void Lox::run(std::string source) {
+  std::cout << source << std::endl;
+}
+
+void Lox::run_prompt(void) {
+  std::string input;
+
+  for (;;) {
+    std::cout << "> ";
+    std::getline(std::cin, input);
+    if (input == "")
+      break;
+    run(input);
+  }
+}
+
+void Lox::run_file(std::string path) {
+  run(load_file(path));
+}
+
+static std::string load_file(std::string path) {
+  std::ifstream file_stream(path);
+  std::stringstream str_stream;
+  str_stream << file_stream.rdbuf();
+  return str_stream.str();
 }
--- src/lox.h
+++ src/lox.h
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <string>
+
 class Lox {
  public:
   static void main(int argc, char* argv[]);
+
+ private:
+  static void run(std::string source);
+  static void run_prompt(void);
+  static void run_file(std::string path);
 };
