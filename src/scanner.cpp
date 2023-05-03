2eec31a50f36937e4bac00011f702ad67ea9bbf7
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-05-03 22:40:37 +0200

    more debug info

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -12,7 +12,6 @@ std::vector<Token> Scanner::scan_tokens() {
   while (!is_at_end()) {
     start = current;
     scan_token();
-    std::cout << "scanning" << std::endl;
   }
   tokens.push_back(Token(TokenType::EOF_, "", "", line));
   return tokens;
@@ -20,18 +19,39 @@ std::vector<Token> Scanner::scan_tokens() {
 
 void Scanner::scan_token() {
   char c = advance();
+  std::cout << "scanning " + std::string(1, c) << std::endl;
 
   switch (c) {
-		case '(': add_token(TokenType::LEFT_PAREN); break;
-		case ')': add_token(TokenType::RIGHT_PAREN); break;
-		case '{': add_token(TokenType::LEFT_BRACE); break;
-		case '}': add_token(TokenType::RIGHT_BRACE); break;
-		case ',': add_token(TokenType::COMMA); break;
-		case '.': add_token(TokenType::DOT); break;
-		case '-': add_token(TokenType::MINUS); break;
-		case '+': add_token(TokenType::PLUS); break;
-		case ';': add_token(TokenType::SEMICOLON); break;
-		case '*': add_token(TokenType::STAR); break;
+    case '(':
+      add_token(TokenType::LEFT_PAREN);
+      break;
+    case ')':
+      add_token(TokenType::RIGHT_PAREN);
+      break;
+    case '{':
+      add_token(TokenType::LEFT_BRACE);
+      break;
+    case '}':
+      add_token(TokenType::RIGHT_BRACE);
+      break;
+    case ',':
+      add_token(TokenType::COMMA);
+      break;
+    case '.':
+      add_token(TokenType::DOT);
+      break;
+    case '-':
+      add_token(TokenType::MINUS);
+      break;
+    case '+':
+      add_token(TokenType::PLUS);
+      break;
+    case ';':
+      add_token(TokenType::SEMICOLON);
+      break;
+    case '*':
+      add_token(TokenType::STAR);
+      break;
   }
 }
 
