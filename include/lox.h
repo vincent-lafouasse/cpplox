6eb6137dede3d9e549fc6237e49b84eda0c003c0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-28 15:50:14 +0100

    minor refactor

--- include/lox.h
+++ include/lox.h
@@ -6,13 +6,15 @@ class Lox
 {
    public:
     static void main(int argc, char* argv[]);
-    static void error(int line, std::string message);
+    static void error(int line, const std::string& message);
 
    private:
     static bool had_error;
 
     static void run(std::string source);
     static void run_prompt(void);
-    static void run_file(std::string path);
-    static void report(int line, std::string where, std::string message);
+    static void run_file(const std::string& path);
+    static void report(int line,
+                       const std::string& where,
+                       const std::string& message);
 };
--- src/lox.cpp
+++ src/lox.cpp
@@ -9,7 +9,7 @@
 
 #include "scanner.h"
 
-static std::string load_file(std::string path);
+static std::string load_file(const std::string& path);
 
 bool Lox::had_error = false;
 
@@ -56,7 +56,7 @@ void Lox::run_prompt(void)
     }
 }
 
-void Lox::run_file(std::string path)
+void Lox::run_file(const std::string& path)
 {
     run(load_file(path));
     if (had_error)
@@ -65,19 +65,19 @@ void Lox::run_file(std::string path)
     }
 }
 
-void Lox::error(int line, std::string message)
+void Lox::error(int line, const std::string& message)
 {
     report(line, "", message);
 }
 
-void Lox::report(int line, std::string where, std::string message)
+void Lox::report(int line, const std::string& where, const std::string& message)
 {
     std::cerr << "[line " << line << "] Error" + where + ": " + message
               << std::endl;
     had_error = true;
 }
 
-static std::string load_file(std::string path)
+static std::string load_file(const std::string& path)
 {
     std::ifstream file_stream(path);
     std::stringstream str_stream;
