c0962c1d94f6875e09a1bbff850188fc6005ff63
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-28 16:57:35 +0100

    implement type dispatching for printing

--- include/Literal.h
+++ include/Literal.h
@@ -1,12 +1,19 @@
 #pragma once
 
+#include <iomanip>
+#include <iostream>
 #include <string>
 #include <variant>
 
+#define STRING_INDEX 0
+#define DOUBLE_INDEX 1
+
 class Literal
 {
-	Literal(const std::string& string_literal);
-	Literal(double x);
-private:
-	std::variant<std::string, double> data;
+    Literal(const std::string& string_literal) : data(string_literal){};
+    Literal(double x) : data(x){};
+    friend std::ostream& operator<<(std::ostream& os, const Literal& literal);
+
+   private:
+    std::variant<std::string, double> data;
 };
--- src/Literal.cpp
+++ src/Literal.cpp
@@ -1,1 +1,17 @@
 #include "Literal.h"
+
+std::ostream& operator<<(std::ostream& os, const Literal& literal)
+{
+    switch (literal.data.index())
+    {
+        case STRING_INDEX:
+            os << std::get<STRING_INDEX>(literal.data);
+            break;
+        case DOUBLE_INDEX:
+            os << std::get<DOUBLE_INDEX>(literal.data);
+            break;
+        default:
+            break;
+    }
+    return os;
+}
